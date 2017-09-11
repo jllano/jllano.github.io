4062e979713e053576a7e9bb6c0c135b26347e9b
Joel C. Llano <oopcreator@gmail.com>
2017-09-11 17:46:41 +0800

    Adding marker

--- js/view.js
+++ js/view.js
@@ -26,8 +26,39 @@
             showRestaurants: function () {
                 var map = new google.maps.Map(that.map, {
                   center: that.latLng,
-                  zoom: 14
+                  zoom: 15
                 });
+
+                var infowindow = new google.maps.InfoWindow();
+                var service = new google.maps.places.PlacesService(map);
+                
+                service.nearbySearch({
+                  location: that.latLng,
+                  radius: 5000,
+                  type: ['Restaurants']
+                }, callback);
+
+
+                function callback(results, status) {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        for (var i = 0; i < results.length; i++) {
+                            createMarker(results[i]);
+                        }
+                    }
+                }
+
+                function createMarker(place) {
+                    var placeLoc = place.geometry.location;
+                    var marker = new google.maps.Marker({
+                    map: map,
+                        position: place.geometry.location
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.setContent(place.name);
+                        infowindow.open(map, this);
+                    });
+                }
             }
         };
 
