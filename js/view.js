f47252d3e0cd791838aa613cc771924c3b38c9ba
Joel C. Llano <oopcreator@gmail.com>
2017-09-14 21:44:14 +0800

    Initialize drawing manager

--- js/view.js
+++ js/view.js
@@ -261,8 +261,8 @@
             drawingMode: google.maps.drawing.OverlayType.MARKER,
             drawingControl: true,
             drawingControlOptions: {
-              position: google.maps.ControlPosition.TOP_CENTER,
-              drawingModes: ['rectangle']
+              position: google.maps.ControlPosition.BOTTOM_CENTER,
+              drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
             },
             markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
             circleOptions: {
@@ -277,14 +277,6 @@
         
         drawingManager.setMap(map);
 
-        drawingManager.setOptions({
-          drawingControlOptions: {
-            position: google.maps.ControlPosition.BOTTOM_CENTER,
-            drawingModes: ['marker']
-          }
-        });
-
-        // To show:
         drawingManager.setOptions({
             drawingControl: true
         });
